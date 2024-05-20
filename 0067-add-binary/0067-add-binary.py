0778124e3320c8d5d39cf804dde1c155a935dbaf
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-05-20 19:06:39 +0300

    Time: 33 ms (82.31%), Space: 16.5 MB (70.71%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,17 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        lenA = len(a)
-        lenB = len(b)
-        numA = 0
-        numB = 0
         
-        for i in range(lenA):
-            numA += int(a[i])* (2**(lenA-1-i))
-                                
-        for i in range(lenB):
-            numB += int(b[i])* (2**(lenB-1-i))
         
-        sumAB = numA + numB
+        sumAB = int(a, 2) + int(b, 2)
         return str("{0:b}".format(int(sumAB)))
             
             
