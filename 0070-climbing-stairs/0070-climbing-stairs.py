844a890715167b0b8e742f2557ede21ba85ff0fd
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-05-20 19:49:13 +0300

    Time: 30 ms (83.27%), Space: 16.5 MB (30.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,4 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        ratio =  (1+(5**0.5))/2
-        return int(((ratio**(n+1)) - (1-ratio)**(n+1))/(5**0.5))
+        return int(((((1+(5**0.5))/2)**(n+1)) - (1-((1+(5**0.5))/2))**(n+1))/(5**0.5))
         
