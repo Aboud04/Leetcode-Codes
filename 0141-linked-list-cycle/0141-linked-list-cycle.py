7fb334a11cfcf2b16f5a3e3f85ca56606dcfe80f
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-05-21 10:39:59 +0300

    Time: 38 ms (91.25%), Space: 19.8 MB (8.45%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        def recurse(head1,head2):
+            if not head1 or not head2 or not head2.next: return False
+            if head1 == head2: return True
+            return recurse(head1.next,head2.next.next)
+        
+        if not head:
+            return False
+        return recurse(head,head.next)
+            
+        
+        
