c2dad761cb821536c4fbcecdd6f73a1a827c44b3
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-05-21 22:39:47 +0300

    Time: 2869 ms (5.59%), Space: 27.5 MB (21.92%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        
+        arr = []
+        def createCopy(listNode):
+            if not listNode:
+                return None
+            
+            cpy_head = ListNode(listNode.val)
+            original_ptr = listNode
+            cpy_ptr = cpy_head
+    
+            while original_ptr:
+                if original_ptr in arr:
+                    return original_ptr
+                arr.append(original_ptr)
+                original_ptr = original_ptr.next
+                if original_ptr:
+                    cpy_ptr.next = ListNode(original_ptr.val)
+                    cpy_ptr = cpy_ptr.next
+
+            return None
+        
+        if headA == headB:
+            return headA
+        createCopy(headA)
+        return createCopy(headB)
+         
+                
+                
