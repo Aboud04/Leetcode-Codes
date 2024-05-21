0588453303e68089650f1ecd4cbe8d9bea753e89
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-05-21 10:41:22 +0300

    Time: 32 ms (98.72%), Space: 19.9 MB (8.45%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -8,7 +8,7 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
         def recurse(head1,head2):
-            if  not head2 or not head2.next: return False
+            if not head1 or not head2 or not head2.next: return False
             if head1 == head2: return True
             return recurse(head1.next,head2.next.next)
         
