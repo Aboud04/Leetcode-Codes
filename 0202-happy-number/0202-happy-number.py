548f95e7d4d01044d7bddd1dbe815189cf904b9f
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2024-05-24 00:27:57 +0300

    Time: 37 ms (56.14%), Space: 16.6 MB (38.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         
-            
+        cycle_members = {4, 16, 37, 58, 89, 145, 42, 20,85}
+        
         def sumSquare(n):
             total = 0
             while n > 0:
@@ -12,5 +13,5 @@ class Solution:
         while True:
             n = sumSquare(n)
             if n == 1: return True
-            if n == 89: return False
+            if n in cycle_members: return False
             
