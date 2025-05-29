6574c9c62e89187df66c595db8c29220176b867f
Abdulrahman Al-Taweel <134710122+Aboud04@users.noreply.github.com>
2025-05-29 16:56:05 +0300

    Time: 0 ms (100%), Space: 17.7 MB (57.83%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,18 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-       
-
-        Pascal = []
-
-        for i in range(numRows):
-            row = []
-            for j in range(i + 1):
-                row.append(int(math.factorial(i) / (math.factorial(j) * math.factorial(i - j))))
-            Pascal.append(row)
-
-        return Pascal
-
+        if numRows == 1:
+            return [[1]]
+        elif numRows == 2:
+            return [[1], [1,1]]
+        l = [[1], [1, 1]]
+        for i in range(2, numRows + 1):
+            ll = []
+            for j in range(i):
+                if j == 0 or j == i - 1:
+                    ll.extend([1])
+                else:
+                    ll.extend([l[i-1][j-1] + l[i-1][j]])
+            l.extend([ll])
+        return [l[0]] + l[2:]
 
         
